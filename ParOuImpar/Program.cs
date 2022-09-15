e86c9fb803529c2694c43179acb75f763212b2c1
Erismar Oliveira <74121763+erismaroliveira@users.noreply.github.com>
2022-09-15 10:46:48 -0300

    Initial commit

---
+++ ParOuImpar/Program.cs
@@ -0,0 +1,28 @@
+﻿/* 
+* Programa C# para verificar se o número digitado é par ou ímpar
+*/ 
+
+using System;
+
+namespace ParOuImpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            Console.Write("Digite um número: ");
+            numero = int.Parse(Console.ReadLine());
+            if (numero % 2 == 0)
+            {
+                Console.Write("Número inserido é um número par");
+                Console.Read();
+            }
+            else
+            {
+                Console.Write("O número digitado é um número ímpar");
+                Console.Read();
+            }
+        }
+    }
+}
